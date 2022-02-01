64fd16beaf9e3f177eac10f589a341e5228a02ad
Sam Cowley <sfcowley@gmail.com>
2022-02-01 08:03:43 +0000

    added new require statement to gatsby-config to get over esm issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
-import dotenv from 'dotenv';
-
-dotenv.config({ path: '.env' });
+require('dotenv').config({
+        path: `.env.${process.env.NODE_ENV}`,
+});
 
 export default {
         // siteMetadata: {
