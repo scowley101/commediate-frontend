f685cd2c944f9183f52253c5b2a6d1b16463586e
Sam Cowley <sfcowley@gmail.com>
2022-02-01 09:03:05 +0000

    Deleted and re added Header.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,21 +1,21 @@
 import React, { useState } from 'react';
-// import Header from './Header';
 import Footer from './Footer';
 import GlobalStyles from '../styles/GlobalStyles';
 import Typography from '../styles/Typography';
+import Header from './Header';
 
 function Layout(props) {
         const { children, onHideNav, onShowNav, showNav, siteTitle, navMenuItems } = props;
 
         return (
                 <>
-                        {/* <Header
+                        <Header
                                 navMenuItems={navMenuItems}
                                 siteTitle={siteTitle}
                                 onHideNav={onHideNav}
                                 onShowNav={onShowNav}
                                 showNav={showNav}
-                        /> */}
+                        />
                         <>{children}</>
                         <Footer
                                 navMenuItems={navMenuItems}
