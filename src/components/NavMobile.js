bb96968b9d51a86d4fa93a0edbe5b5f76c52a87f
Sam Cowley <sfcowley@gmail.com>
2022-02-06 18:11:04 +0000

    added overflow hidden to navmobile container

--- src/components/NavMobile.js
+++ src/components/NavMobile.js
@@ -7,30 +7,33 @@ import { SiteContext } from './context/SiteContext';
 import NavIcon from './NavIcon';
 
 const NavMobileStyles = styled.div`
-        z-index: 98;
-        height: 100vh;
-        width: 100%;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        text-align: center;
-        background: var(--cmGrey);
-        position: absolute;
-        top: 0;
-        right: 0;
-        transition: transform 300ms;
-        transform: ${({ isNavOpen }) => (isNavOpen ? 'translateX(0)' : 'translateX(100%)')};
+        overflow: hidden;
+        .nav-container {
+                z-index: 98;
+                height: 100vh;
+                width: 100%;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                text-align: center;
+                background: var(--cmGrey);
+                position: absolute;
+                top: 0;
+                right: 0;
+                transition: transform 300ms;
+                transform: ${({ isNavOpen }) => (isNavOpen ? 'translateX(0)' : 'translateX(100%)')};
 
-        ul {
-                list-style-type: none;
-                margin: 0;
-                padding: 0;
-        }
+                ul {
+                        list-style-type: none;
+                        margin: 0;
+                        padding: 0;
+                }
 
-        li {
-                margin-top: 1rem;
-                color: var(--cmBlue);
+                li {
+                        margin-top: 1rem;
+                        color: var(--cmBlue);
+                }
         }
 `;
 
@@ -41,15 +44,17 @@ export default function NavMobile({ className, showNav, siteTitle, navMenuItems 
                 <>
                         <NavIcon />
                         <NavMobileStyles isNavOpen={isNavOpen}>
-                                {showNav && navMenuItems && (
-                                        <ul>
-                                                {navMenuItems.map((i) => (
-                                                        <li className="text-lg">
-                                                                <CTALink {...i} />
-                                                        </li>
-                                                ))}
-                                        </ul>
-                                )}
+                                <div className="nav-container">
+                                        {showNav && navMenuItems && (
+                                                <ul>
+                                                        {navMenuItems.map((i) => (
+                                                                <li className="text-lg">
+                                                                        <CTALink {...i} />
+                                                                </li>
+                                                        ))}
+                                                </ul>
+                                        )}
+                                </div>
                         </NavMobileStyles>
                 </>
         );
